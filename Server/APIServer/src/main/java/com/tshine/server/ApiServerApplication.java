761ff6ddc9839f69106d8ad1c118d522899318d3
DuyTQ <duytq@seatech.com.vn>
2023-03-26 12:03:12 +0700

    fix entity scan

--- Server/APIServer/src/main/java/com/tshine/server/ApiServerApplication.java
+++ Server/APIServer/src/main/java/com/tshine/server/ApiServerApplication.java
@@ -2,8 +2,10 @@ package com.tshine.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.tshine.common.entities")
 public class ApiServerApplication {
 
     public static void main(String[] args) {
