209171cf338790d789d1803f5cad529f81cb6d1b
DuyTQ <duytq@seatech.com.vn>
2024-02-22 15:22:05 +0700

    push SystemFileService

--- Server/Common/src/main/java/com/tshine/common/utils/FileUtils.java
+++ Server/Common/src/main/java/com/tshine/common/utils/FileUtils.java
@@ -1,6 +1,10 @@
 package com.tshine.common.utils;
 
 import com.tshine.common.factory.KeyGenarator;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.NonNull;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -9,6 +13,7 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class FileUtils {
+    private static final Logger logger = LoggerFactory.getLogger(FileUtils.class);
     public static File convertMultiPartToFile(MultipartFile multipartFile) throws IOException {
         File file = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
         FileOutputStream fos = new FileOutputStream(file);
@@ -17,8 +22,47 @@ public class FileUtils {
         return file;
     }
 
-
     public static String generateFileName(String fileName) {
-        return KeyGenarator.getKey() + fileName.replaceAll(" ", "_");
+        return KeyGenarator.getKey() + fileName.replaceAll("\\s+", "_");
+    }
+    public static File createFile(String fileName, String... pathFolder) throws IOException {
+        File folderPath = FileUtils.createFolder(pathFolder);
+        File file = new File(folderPath.getPath() + "/" + generateFileName(fileName));
+        if(file.createNewFile()){
+            return file;
+        }
+        throw new IOException("FileUtils: createFile() ERROR Invalid file path");
+    }
+    private static File createFolder(@NonNull String... rootFolder) {
+        StringBuilder path = new StringBuilder();
+        for (String fd : rootFolder){
+            if(StringUtils.isNotEmpty(fd)){
+                path.append("/").append(fd);
+            }
+        }
+        File folderPath = new File(path.toString());
+        if (!folderPath.isDirectory()) {
+            boolean isCreated = folderPath.mkdir();
+            if(isCreated){
+                FileUtils.chmod(folderPath, "777");
+            }
+        }
+        return folderPath;
+    }
+
+    private static void chmod(File file, String mode) {
+        String os = System.getProperty("os.name");
+        if (os.contains("Windows") || os.contains("windows")) {
+            return;
+        }
+        String cmd = "chmod " + mode + " " + file.getAbsolutePath();
+        try {
+            Runtime rt = Runtime.getRuntime();
+            Process proc = rt.exec(cmd);
+            int waitFor = proc.waitFor();
+            logger.info("===========FileUtils chmod() waitFor= " + waitFor);
+        } catch (Exception e) {
+            logger.error("===========FileUtils chmod() ERROR===========", e);
+        }
     }
 }
--- Server/Service/src/main/java/com/tshine/service/service/impl/SystemFileServiceImpl.java
+++ Server/Service/src/main/java/com/tshine/service/service/impl/SystemFileServiceImpl.java
@@ -1,11 +1,17 @@
 package com.tshine.service.service.impl;
 
 import com.tshine.common.entities.system.SystemFile;
+import com.tshine.common.utils.FileUtils;
 import com.tshine.service.service.SystemFileService;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -13,10 +19,16 @@ import java.util.List;
 
 @Service
 public class SystemFileServiceImpl implements SystemFileService {
+    private final ApplicationContext context;
+
+    public SystemFileServiceImpl(ApplicationContext context) {
+        this.context = context;
+    }
+
     @Override
     public String saveFileToStorage(MultipartFile file, String dir) throws IOException {
-        Path filepath = Paths.get(dir, file.getOriginalFilename());
-        file.transferTo(filepath);
+        File filepath = FileUtils.createFile(file.getOriginalFilename(), dir, "test");
+        FileCopyUtils.copy(file.getInputStream(), Files.newOutputStream(filepath.toPath()));
         return filepath.toString();
     }
 
